dc466f4301d5897f95f1bc07399032c28d4514c5
medghar riad <101431493+riad-medghar@users.noreply.github.com>
2024-11-16 22:39:02 +0100

    Fix routing

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./component/Home"
 function App() { 
   
   return (
-    <Router basename="/PortfolioRiad">
+    <Router >
       <Routes>
         <Route path="/" element={<OnBoarding/>} />
         <Route path="/home" element={<Home/>} />
