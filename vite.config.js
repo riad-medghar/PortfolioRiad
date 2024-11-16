6aca5034ba5a88808a445c0617ff7942e455e21e
medghar riad <101431493+riad-medghar@users.noreply.github.com>
2024-11-16 22:47:43 +0100

    zedt the path in local and for the deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import replace from '@rollup/plugin-replace';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base:'/PortfolioRiad/',
-
-})
+  plugins: [
+    react(),
+    replace({
+      'process.env.BASE_URL': JSON.stringify(process.env.NODE_ENV === 'production' ? '/PortfolioRiad/' : '/'),
+      preventAssignment: true,
+    }),
+  ],
+  base: process.env.NODE_ENV === 'production' ? '/PortfolioRiad/' : '/',
+});
