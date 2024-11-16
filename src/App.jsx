ace81009f59c3b2d07de099b5b1e586c1988a5db
medghar riad <101431493+riad-medghar@users.noreply.github.com>
2024-11-16 23:28:39 +0100

    cr

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import OnBoarding from "./component/OnBoarding";
-import Home from "./component/Home";
-
-function App() {
+import {BrowserRouter as  Router,Routes,Route } from "react-router-dom"
+import OnBoarding from "./component/OnBoarding"
+import Home from "./component/Home"
+function App() { 
+  
   return (
-    <Router basename={process.env.NODE_ENV === 'production' ? "/PortfolioRiad" : "/"}>
+    <Router basename="/PortfolioRiad" >
       <Routes>
-        <Route path="/" element={<OnBoarding />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<OnBoarding/>} />
+        <Route path="/home" element={<Home/>} />
+
       </Routes>
     </Router>
-  );
+      
+    
+    
+    )
 }
 
-export default App;
+export default App
