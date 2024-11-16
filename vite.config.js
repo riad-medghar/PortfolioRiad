d789c52e0fb59dd90da6fe78eddadbc009cfedd1
medghar riad <101431493+riad-medghar@users.noreply.github.com>
2024-11-16 22:17:15 +0100

    Fix routing and base URL welli

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
-import { replaceCodePlugin } from 'vite-plugin-replace';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    replaceCodePlugin({
-      replacements: [
-        {
-          from: '%BASE_URL%',
-          to: process.env.NODE_ENV === 'production' ? '/PortfolioRiad/' : '/',
-        },
-      ],
-    }),
-  ],
-  base: process.env.NODE_ENV === 'production' ? '/PortfolioRiad/' : '/',
-});
+  plugins: [react()],
+  base:'/PortfolioRiad/',
+
+})
