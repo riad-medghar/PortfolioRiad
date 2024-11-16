3c6442d46de6d0f07936a428360d27a54724f44a
medghar riad <101431493+riad-medghar@users.noreply.github.com>
2024-11-16 22:55:42 +0100

    w drwk?

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import replace from '@rollup/plugin-replace';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    replace({
-      'process.env.BASE_URL': JSON.stringify(process.env.NODE_ENV === 'production' ? '/PortfolioRiad/' : '/'),
-      preventAssignment: true,
-    }),
-  ],
-  base: process.env.NODE_ENV === 'production' ? '/PortfolioRiad/' : '/',
-});
+  plugins: [react()],
+  base:'/PortfolioRiad/',
+
+})
