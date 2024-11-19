312645aeba107667d424b044b04c705a5400f3a2
medghar riad <101431493+riad-medghar@users.noreply.github.com>
2024-11-19 23:19:30 +0100

    adding some responsive styling to the website

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,6 +1,6 @@
 import animation from "../assets/animation/Animation - 1731279373321.webm";
 import animationse from "../assets/animation/Animation - 1731279373321.gif";
-import {FaFacebook, FaInstagram, FaLinkedin, FaTwitter, FaGithub} from "react-icons/fa";
+import {FaFacebook, FaInstagram, FaLinkedin, FaTwitter, FaGithub, FaTimes, FaBars} from "react-icons/fa";
 import React, {useEffect, useState} from "react";
 import bgimage from "../assets/img/background-image/Animation - 1731402838666.json";
 import Lottie from "lottie-react";
@@ -12,6 +12,7 @@ function Home() {
     const [message,setMessage]=useState("");
     const [currentServiceIndex, setCurrentServiceIndex] = useState(0);
     const [email ,setEmail] = useState("");
+    const [isMobileMenuOpen , setIsMobileMenuOpen]=useState(false);
     const [isModalOpen,setIsModalOpen]=useState(false);
     const navLinks = [
         {href: "home", title: "Home"},
@@ -98,13 +99,16 @@ function Home() {
         }
       );
     }
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen((prev) => !prev);
+      };
 
 
   return (
     <div className="bg-gray-800 min-h-screen pt-4 ">
-        <nav className="felx justify-center space-x-4 p-4 bg-emerald-700 text-white rounded-full 
+        <nav className="flex justify-center space-x-4 p-4 bg-emerald-700 text-white rounded-full 
         mx-auto w-max h-14 my-3 shadow-2xl">
-            <ul className="flex space-x-4">
+            <ul className="hidden md:flex  space-x-4">
                 {navLinks.map((link, index) => (
                     <li key={index}>
                         <a href={`#${link.href}`} className="hover:text-transparent 
@@ -115,8 +119,30 @@ function Home() {
                     </li>
                 ))}
             </ul>
+            <div className="md:hidden ">
+                <button onClick={toggleMobileMenu}>
+                  {isMobileMenuOpen ? <FaTimes size={24} />: <FaBars size={24} />}
+                </button>
+            </div>
         </nav>
-        <div id="home" className="flex justify-center items-center mt-36 space-x-4 relative -top-20 bg-[url('data:image/svg+xml;base64,'+bgimage)]">
+        {isMobileMenuOpen && (
+          <ul className="md:hidden flex flex-col items-center space-y-4 bg-emerald-700
+          text-white p-4 rounded-lg shadow-2xl">
+            {navLinks.map((link, index) => (
+              <li key={index}>
+                <a
+                  href={`#${link.href}`}
+                  className="hover:text-transparent hover:bg-clip-text 
+                  hover:bg-gradient-to-r hover:from-purple-400 hover:to-pink-500 
+                  hover:scale-110 transition-transform duration-300 hover:text-lg"
+                >
+                  {link.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+      )}
+        <div id="home" className="flex flex-col md:flex-row justify-center items-center mt-36 space-y-4 md:space-x-4 md:space-y-0  relative -top-20 bg-[url('data:image/svg+xml;base64,'+bgimage)]">
      
             <div className="bg-gray-900 text-green-500 p-6 rounded-lg shadow-lg w-96 h-96 font-mono border border-gray-700">
                 {/* Terminal Header */}
@@ -166,9 +192,9 @@ function Home() {
         </div>
         <div className="border-t border-gray-600 my-2 mx-4"></div>
         <div id="about" className="my-16">
-        <div className="flex justify-center items-center  space-x-4">
+        <div className="flex flex-col md:flex-row justify-center items-center  space-y-4 md:space-y-0 md:space-x-4">
           {/* Left Box */}
-          <div className="bg-gray-900 text-white p-6 rounded-lg shadow-lg w-1/2 h-56">
+          <div className="bg-gray-900 text-white p-6 rounded-lg shadow-lg w-full md:w-1/2 min-h-56 ">
             <div className="text-center">
               <h2 className="text-xl font-bold mt-7 mb-16">Languages I Know</h2>
               <div className="mt-10">
@@ -186,7 +212,7 @@ function Home() {
             </div>
           </div>
           {/* Right Box */}
-          <div className="bg-gray-900 text-white p-6 rounded-lg shadow-lg w-2/5 ">
+          <div className="bg-gray-900 text-white p-6 rounded-lg shadow-lg w-full md:w-2/5  min-h-56">
             <div className="text-center">
               <h2 className="text-xl font-bold">About Me</h2>
               <p className="mt-2">
@@ -199,15 +225,17 @@ function Home() {
         </div>
       </div>
       <div className="border-t border-gray-600 my-2 mx-4"></div>
-      <div id="services" className="flex flex-col bg-gray-900 h-96">
-        <h1 className="flex justify-center items-center mt-3 font-extrabold italic text-5xl bg-gradient-to-r from-blue-500 to-green-500 bg-clip-text text-transparent mb-4 ">Services</h1>
-        <div className="relative overflow-hidden h-full">
+      <div id="services" className="flex flex-col bg-gray-900 min-h-screen md:h-96">
+        <h1 className="flex justify-center items-center mt-3 font-extrabold italic 
+       text-3xl  md:text-5xl bg-gradient-to-r from-blue-500 to-green-500 bg-clip-text 
+        text-transparent mb-4 ">Services</h1>
+        <div className="relative overflow-hidden flex-grow">
           <div className="absolute inset-0 flex transition-transform duration-1000" style={{ transform: `translateX(-${currentServiceIndex * 100}%)` }}>
             {services.map((service, index) => (
               <div key={index} className="bg-gray-900 text-white p-6 flex-shrink-0 w-full h-full flex items-center justify-center">
                 <div className="text-center">
                   <h2 className="text-2xl font-bold text-blue-500">{service.title}</h2>
-                  <p className="mt-2 w-96 bg-gradient-to-b from-lime-700 via-teal-600 to-blue-300 bg-clip-text text-transparent">{service.description}</p>
+                  <p className="mt-2 w-full md:w-96 bg-gradient-to-b from-lime-700 via-teal-600 to-blue-300 bg-clip-text text-transparent">{service.description}</p>
                 </div>
               </div>
             ))}
@@ -223,105 +251,103 @@ function Home() {
             </button>
           </div>
         </div>
-      </div>
-      <div className="border-t border-gray-600 my-2 mx-4"></div>
-      <div id="contact" className="flex flex-col justify-center items-center">
-      <h2 className=" font-bold mb-6 text-3xl mt-5 bg-gradient-to-r from-emerald-600 to-teal-400 text-transparent bg-clip-text">Contact Me</h2>
-      
-      <div className="flex w-full justify-center items-center space-x-4">
-          <div className="bg-gray-900 text-white p-6 rounded-lg shadow-lg w-1/2 flex flex-col justify-center items-center">
-            <h2 className="text-2xl font-bold mb-4">Social Media</h2>
-            <ul className="space-y-4">
-              <li>
-                <a href="https://www.facebook.com/yourprofile" className="flex items-center space-x-2 text-blue-500 hover:underline">
-                  <FaFacebook className="w-6 h-6" />
-                  <span>Facebook</span>
-                </a>
-              </li>
-              <li>
-                <a href="https://www.instagram.com/yourprofile" className="flex items-center space-x-2 text-pink-500 hover:underline">
-                  <FaInstagram className="w-6 h-6" />
-                  <span>Instagram</span>
-                </a>
-              </li>
-              <li>
-                <a href="https://www.linkedin.com/in/yourprofile" className="flex items-center space-x-2 text-blue-700 hover:underline">
-                  <FaLinkedin className="w-6 h-6" />
-                  <span>LinkedIn</span>
-                </a>
-              </li>
-              <li>
-                <a href="https://twitter.com/yourprofile" className="flex items-center space-x-2 text-blue-400 hover:underline">
-                  <FaTwitter className="w-6 h-6" />
-                  <span>Twitter</span>
-                </a>
-              </li>
-              <li>
-                <a href="https://github.com/yourprofile" className="flex items-center space-x-2 text-gray-900 hover:underline">
-                  <FaGithub className="w-6 h-6" />
-                  <span>GitHub</span>
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className="bg-gray-900 text-white p-6 rounded-lg shadow-lg w-2/5 mb-7">
-            <h2 className="text-2xl font-bold mb-4 flex justify-center">Write Me a letter</h2>
-            <form onSubmit={handleSendEmail}>
-              <div className="mb-4">
-                <label htmlFor="userEmail" className="block text-white mb-2">Your Email</label>
-                <input 
-                  type="email" 
-                  id="userEmail" 
-                  className="w-full p-2 bg-gray-800 text-white rounded-lg focus:outline-none"
-                  value={email} 
-                  onChange={(e)=>setEmail(e.target.value)}
-                  required />
-              </div>
-              <div className="mb-4">
-                <label htmlFor="message" className="block text-white mb-2">Your Message</label>
-                <textarea 
-                  id="message" 
-                  className="w-full p-2 bg-gray-800 text-white rounded-lg focus:outline-none" 
-                  rows="4" 
-                  value={inputValue}
-                  onChange={(e)=>setInputValue(e.target.value)}
-                  required></textarea>
-              </div>
-              <button type="submit" className="bg-emerald-700 text-white py-2 px-4 rounded-lg hover:bg-emerald-600 transition-colors duration-300">Send</button>
-            </form>
-          </div>
+      </div><div className="border-t border-gray-600 my-2 mx-4"></div>
+<div id="contact" className="flex flex-col justify-center items-center">
+  <h2 className="font-bold mb-6 text-3xl mt-5 bg-gradient-to-r from-emerald-600 to-teal-400 text-transparent bg-clip-text">Contact Me</h2>
+  
+  <div className="flex flex-col md:flex-row w-full justify-center items-center space-y-4 md:space-y-0 md:space-x-4">
+    <div className="bg-gray-900 text-white p-6 rounded-lg shadow-lg w-full md:w-1/2 flex flex-col justify-center items-center">
+      <h2 className="text-2xl font-bold mb-4">Social Media</h2>
+      <ul className="space-y-4">
+        <li>
+          <a href="https://www.facebook.com/yourprofile" className="flex items-center space-x-2 text-blue-500 hover:underline">
+            <FaFacebook className="w-6 h-6" />
+            <span>Facebook</span>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/yourprofile" className="flex items-center space-x-2 text-pink-500 hover:underline">
+            <FaInstagram className="w-6 h-6" />
+            <span>Instagram</span>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/yourprofile" className="flex items-center space-x-2 text-blue-700 hover:underline">
+            <FaLinkedin className="w-6 h-6" />
+            <span>LinkedIn</span>
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com/yourprofile" className="flex items-center space-x-2 text-blue-400 hover:underline">
+            <FaTwitter className="w-6 h-6" />
+            <span>Twitter</span>
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/yourprofile" className="flex items-center space-x-2 text-gray-900 hover:underline">
+            <FaGithub className="w-6 h-6" />
+            <span>GitHub</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+    <div className="bg-gray-900 text-white p-6 rounded-lg shadow-lg w-full md:w-2/5 mb-7">
+      <h2 className="text-2xl font-bold mb-4 flex justify-center">Write Me a letter</h2>
+      <form onSubmit={handleSendEmail}>
+        <div className="mb-4">
+          <label htmlFor="userEmail" className="block text-white mb-2">Your Email</label>
+          <input 
+            type="email" 
+            id="userEmail" 
+            className="w-full p-2 bg-gray-800 text-white rounded-lg focus:outline-none"
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)}
+            required />
         </div>
-     </div>
-    {isModalOpen && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-          <div className="bg-white p-6 rounded-lg shadow-lg w-96 relative">
-          <button
-              className="absolute top-2 right-2 text-gray-700 hover:text-gray-900"
-              onClick={() => setIsModalOpen(false)}
-            >
-              &times;
-            </button>
-            <h2 className="text-2xl font-bold mb-4">Enter Your Email</h2>
-            <form onSubmit={handleSendEmail}>
-              <div className="mb-4">
-                <label htmlFor="email" className="block text-gray-700 mb-2">Email</label>
-                <input
-                  type="email"
-                  id="email"
-                  className="w-full p-2 bg-gray-200 text-gray-700 rounded-lg focus:outline-none"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                />
-              </div>
-              <button type="submit" className="bg-emerald-700 text-white py-2 px-4 rounded-lg hover:bg-emerald-600 transition-colors duration-300">
-                Send
-              </button>
-            </form>
-          </div>
+        <div className="mb-4">
+          <label htmlFor="message" className="block text-white mb-2">Your Message</label>
+          <textarea 
+            id="message" 
+            className="w-full p-2 bg-gray-800 text-white rounded-lg focus:outline-none" 
+            rows="4" 
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            required></textarea>
         </div>
-    )}
-     
+        <button type="submit" className="bg-emerald-700 text-white py-2 px-4 rounded-lg hover:bg-emerald-600 transition-colors duration-300">Send</button>
+      </form>
+    </div>
+  </div>
+</div>
+{isModalOpen && (
+  <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+    <div className="bg-white p-6 rounded-lg shadow-lg w-96 relative">
+      <button
+        className="absolute top-2 right-2 text-gray-700 hover:text-gray-900"
+        onClick={() => setIsModalOpen(false)}
+      >
+        &times;
+      </button>
+      <h2 className="text-2xl font-bold mb-4">Enter Your Email</h2>
+      <form onSubmit={handleSendEmail}>
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-gray-700 mb-2">Email</label>
+          <input
+            type="email"
+            id="email"
+            className="w-full p-2 bg-gray-200 text-gray-700 rounded-lg focus:outline-none"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit" className="bg-emerald-700 text-white py-2 px-4 rounded-lg hover:bg-emerald-600 transition-colors duration-300">
+          Send
+        </button>
+      </form>
+    </div>
+  </div>
+)}
     </div>
   );
 }
